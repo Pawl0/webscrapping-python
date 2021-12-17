01db37845c8c7e3be492a765db71f9a604500b61
Pawl0 <paulohsantosol@gmail.com>
2021-12-17 00:37:00 -0300

    fix: element xpaths and add exception handling

--- kabum/kabum.py
+++ kabum/kabum.py
@@ -11,29 +11,39 @@ option.headless = True
 driver = webdriver.Firefox(options=option)
 
 driver.get(url)
-driver.implicitly_wait(30)  # in seconds
+driver.implicitly_wait(20)  # in seconds
 
 itens = []
 
-for elementIndex in range(10):
-    element = driver.find_element_by_xpath(
-        f"//html//body//div[1]//main//div[1]//article//section[1]//div[1]//div[2]//div[1]//div//div//div//div[{elementIndex+1}]//div//div//div//a//h2")
-    elementPrice = driver.find_element_by_xpath(
-        f"//html//body//div[1]//main//div[1]//article//section[1]//div[1]//div[2]//div[1]//div//div//div//div[{elementIndex+1}]//div//div//div//a//div//span[2]")
-    html_content = element.get_attribute('innerHTML')
-    html_price = elementPrice.get_attribute('innerHTML')
+try:
+    for elementIndex in range(10):
+        elementDescription = driver.find_element_by_xpath(
+            f"//html//body//div//main//div//article//section[1]//div[1]//div[2]//div[1]//div//div//div//div[{elementIndex+1}]//div//div//a//div//div[1]//h2")
+        elementPrice = driver.find_element_by_xpath(
+            f"//html//body//div//main//div//article//section[1]//div[1]//div[2]//div[1]//div//div//div//div[{elementIndex+1}]//div//div//a//div//div[2]//span[2]")
 
-    price = html_price.replace('&nbsp;', ' ')
+        html_content = elementDescription.get_attribute('innerHTML')
+        html_price = elementPrice.get_attribute('innerHTML')
 
-    itens.insert(elementIndex, {
-        "item": html_content,
-        "price": price
-    })
+        price = html_price.replace('&nbsp;', ' ')
 
-driver.quit()
+        print(html_content)
+        print(price)
 
-print(itens)
+        itens.append({
+            "item": html_content,
+            "price": price
+        })
 
-with open('kabum-top-10.json', 'w', encoding='utf-8') as jp:
-    js = json.dumps(itens, indent=4)
-    jp.write(js)
+    driver.quit()
+
+    print(itens)
+
+    with open('kabum-top-10.json', 'w', encoding='utf-8') as jp:
+        js = json.dumps(itens, indent=4)
+        jp.write(js)
+
+except error:
+    print(error)
+    driver.close()
+    driver.quit()

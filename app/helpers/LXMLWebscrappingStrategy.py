eb1b1728d0ee49bfd122d254b2056b55213f5c69
Pawl0 <paulohsantosol@gmail.com>
2022-03-13 02:14:00 -0300

    chore: add prints for debug of lxml

--- app/helpers/LXMLWebscrappingStrategy.py
+++ app/helpers/LXMLWebscrappingStrategy.py
@@ -10,11 +10,13 @@ class LXMLWebscrappingStrategy(WebscrappingStrategy):
 
     def setupInternals(self):
         header = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest"
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"
         }
         response = requests.get(self.url, headers=header)
+        print(response)
         self.tree = html.fromstring(response.content)
+        print(self.tree)
 
     def getElementByXpath(self, xpath):
+        print(xpath)
         return str(self.tree.xpath(xpath+"/text()"))

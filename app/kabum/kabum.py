75a988705cf82a7122e4b4dbe00e0559dc6678a0
Pawl0 <paulohsantosol@gmail.com>
2022-03-13 02:03:32 -0300

    chore: clean up slashs from kabum url

--- app/kabum/kabum.py
+++ app/kabum/kabum.py
@@ -10,6 +10,6 @@ class KabumWebscrapper(Webscrapper):
 
     def getElementsXpathByIndex(self, elementIndex):
         return {
-            "item": f"//html//body//div//main//div//article//section[1]//div[1]//div[2]//div[1]//div//div//div//div[{elementIndex+1}]//div//div//a//div//div[1]//h2",
-            "price": f"//html//body//div//main//div//article//section[1]//div[1]//div[2]//div[1]//div//div//div//div[{elementIndex+1}]//div//div//a//div//div[2]//span[2]"
+            "item": f"/html/body/div/main/div/article/section[1]/div[1]/div[2]/div[1]/div/div/div/div[{elementIndex+1}]/div/div/a/div/div[1]/h2",
+            "price": f"/html/body/div/main/div/article/section[1]/div[1]/div[2]/div[1]/div/div/div/div[{elementIndex+1}]/div/div/a/div/div[2]/span[2]"
         }

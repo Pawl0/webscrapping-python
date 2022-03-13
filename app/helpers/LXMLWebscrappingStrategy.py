8128a8b94c261a0e9b105ad00247ad9d611009af
Pawl0 <paulohsantosol@gmail.com>
2022-03-13 02:26:33 -0300

    chore: add more headers

--- app/helpers/LXMLWebscrappingStrategy.py
+++ app/helpers/LXMLWebscrappingStrategy.py
@@ -10,7 +10,14 @@ class LXMLWebscrappingStrategy(WebscrappingStrategy):
 
     def setupInternals(self):
         header = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+           'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
+            'AppleWebKit/537.11 (KHTML, like Gecko) '
+            'Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'
         }
         response = requests.get(self.url, headers=header)
         print(response)

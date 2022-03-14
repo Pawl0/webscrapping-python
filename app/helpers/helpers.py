c9d590dc22f7b222d13913901dfd5a1077a70378
Pawl0 <paulohsantosol@gmail.com>
2022-03-13 22:57:57 -0300

    fix: indentation

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -31,13 +31,13 @@ class DriverManager:
             FF_options = webdriver.FirefoxOptions()
             FF_profile = webdriver.FirefoxProfile()
             options.headless = True
-	        options.log.level = "trace"
-        	options.add_argument("-remote-debugging-port=9224")
+            options.add_argument("-remote-debugging-port=9224")
+            options.log.level = "trace"
             FF_options.add_argument('--no-sandbox')
             FF_options.add_argument("--headless")
             options.add_argument("--disable-gpu")
             FF_profile.update_preferences()
-	        binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
+            binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
             self.driver = webdriver.Firefox(options=FF_options, firefox_profile=FF_profile, executable_path=os.environ.get('GECKODRIVER_PATH'), firefox_binary=binary))
         else:
             options = Options()

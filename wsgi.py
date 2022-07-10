9d094ca1a1181090b0358d51922524b51d510dc6
Paulo Henrique <paulohsantosol@gmail.com>
2022-07-10 00:16:48 -0300

    feat: add scheduler to run all scrappers everyday

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,28 @@
 from app.main import app
+import schedule
+from WebscrapperFactory import WebscrapperFactory
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+
+webscrapperFactory = WebscrapperFactory()
+makeWebscrapper = webscrapperFactory.makeWebscrapper
+
+def runAll():
+    print("running all scrappers")
+    scrappers = [
+        "epic",
+        "epicV",
+        "prime",
+        "kabum",
+        "indiegala"
+    ]
+    for scrapper in scrappers:
+        print(scrapper)
+        makeWebscrapper(scrapper).execute()
+        time.sleep(10)
+
 if __name__ == "__main__":
+  sched = BackgroundScheduler(daemon=True)
+  sched.add_job(runAll,'interval',days=1)
+  sched.start()
   app.run()

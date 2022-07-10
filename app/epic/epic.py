af427428a43903eaec2a55d84453acece09a28a5
Paulo Henrique <paulohsantosol@gmail.com>
2022-07-09 21:46:51 -0300

    fix: epic web scrapper elements xpath

--- app/epic/epic.py
+++ app/epic/epic.py
@@ -6,7 +6,7 @@ class EpicWebscrapper(Webscrapper):
 
     url = "https://www.epicgames.com/store/pt-BR/"
     _filename = "epic-free-games"
-    total_elements_to_scrappe = 1
+    total_elements_to_scrappe = 2
 
     def __init__(self, webscrappingStrategy):
         self.webscrappingStrategy = webscrappingStrategy
@@ -14,6 +14,6 @@ class EpicWebscrapper(Webscrapper):
 
     def getElementsXpathByIndex(self, elementIndex):
         return {
-            "item": self.webscrappingStrategy.getInnerHTMLByXpath(f'//html//body//div[1]//div//div[4]//main//div[2]//div//div//div//span[5]//div//div//section//div//div[1]//div//div//a//div//div//div[3]//span[1]//div'),
-            "until": self.webscrappingStrategy.getInnerHTMLByXpath(f'//html//body//div[1]//div//div[4]//main//div[2]//div//div//div//span[5]//div//div//section//div//div[1]//div//div//a//div//div//div[3]//span[2]//div//span')
+            "item": self.webscrappingStrategy.getInnerHTMLByXpath(f'//html//body//div[1]//div//div[4]//main//div[2]//div//div//div//span[4]//div//div//section//div//div[{elementIndex+1}]//div//div//a//div//div//div[3]//span[1]//div'),
+            "until": self.webscrappingStrategy.getInnerHTMLByXpath(f'//html//body//div[1]//div//div[4]//main//div[2]//div//div//div//span[4]//div//div//section//div//div[{elementIndex+1}]//div//div//a//div//div//div[3]//span[2]//div//span')
         }
